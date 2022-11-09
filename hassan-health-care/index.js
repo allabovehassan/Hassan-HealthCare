24d8938b24d1d2ab15592ffa6bb5c215e523ce8a
allabovehassan <argalhassan@gmail.com>
2022-11-09 23:16:42 +0530

    day-2 work

---
+++ hassan-health-care/index.js
@@ -0,0 +1,14 @@
+let url = ""
+let out=[]
+
+async function getdata() {
+    try {
+        
+        // let x = await fetch(url)
+        // let out = await x.json()
+ 
+ 
+    } catch {
+        
+    }
+}
